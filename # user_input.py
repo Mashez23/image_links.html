adab1dd79ec0c932fef874cb5e0a9ac826fff936
Mashez23 <158018768+Mashez23@users.noreply.github.com>
2024-02-26 18:48:31 +0300

    Add files via upload

---
+++ # user_input.py
@@ -0,0 +1,7 @@
+#user_input.py
+
+name = input("what is your name?")
+age = (input("How old are you?"))
+location = input("what is your location?")
+
+print("hello(name), you are (age) years old and live in (location)")
